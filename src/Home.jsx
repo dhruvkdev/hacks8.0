652af83a2eb53fed93937b1c0c73b73975d60a1e
Dhruv Kumar <dhruv.k.dev@gmail.com>
2025-10-15 20:23:40 +0530

    impoved01

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,29 +1,101 @@
+import React from "react";
+import { useEffect } from "react";
+import { gsap } from 'gsap'
+import { SteppedEase } from "gsap";
 import './landing.css';
 import sponsors from './assets/sponsors.json';
 import runPikachu from './assets/images/run-pikachu.gif';
-import bikingGirl from './assets/images/bikingGirl.gif'
+import bikingGirl from './assets/images/bikingGirl.gif';
+import logo from './assets/images/logo.png';
+import cloudMov from "./assets/images/cloudMov.png";
+import clouds1 from "./assets/images/clouds1.png";
+import grass from "./assets/images/grass.png";
+
+gsap.registerPlugin(SteppedEase);
 
 const Home = () => {
+  useEffect(() => {
+    gsap.to('.background-layer.ground', {
+      backgroundPosition: '-2048px 0',
+      ease: 'none',
+      duration: 20,
+      repeat: -1
+    });
+    gsap.to(".background-layer.clouds-bg", {
+      backgroundPosition: "-2048px 0",
+      ease: "none",
+      duration: 40,
+      repeat: -1,
+    });
+    gsap.to('.floating-cloud-1', {
+      x: '180vw',
+      duration: 50,
+      repeat: -1,
+      ease: 'none',
+      delay: -5
+    })
+    gsap.to(".floating-cloud-2", {
+      x: "180vw",
+      duration: 70,
+      repeat: -1,
+      ease: "none",
+      delay: -20,
+    });
+    gsap.to(".floating-cloud-3", {
+      x: "180vw",
+      duration: 60,
+      repeat: -1,
+      ease: "none",
+      delay: -35,
+    });
+    gsap.to('.trainer', {
+      y: -5,
+      repeat: -1,
+      yoyo: true,
+      duration: 0.4,
+      ease: 'power1.inOut'
+    });
+    gsap.to('.pikachu', {
+      y: -7,
+      repeat: -1,
+      yoyo: true,
+      duration: 0.35,
+      ease: "power1.inOut",
+    });
+
+  }, []);
   return (
     <>
       <div className="blueBg">
-        <div className="clouds"></div>
-
-        <div className="grass"></div>
-
+        <div
+          className="background-layer clouds-bg"
+          style={{ backgroundImage: `url(${cloudMov})` }}
+        ></div>
+        <div
+          className="background-layer ground"
+          style={{ backgroundImage: `url(${grass})` }}
+        ></div>
+        <img src={clouds1} className="floating-cloud-1 floating-cloud" />
+        <img src={clouds1} className="floating-cloud-2 floating-cloud" />
+        <img src={clouds1} className="floating-cloud-3 floating-cloud" />
+        <h1 className="coming-soon-text" style={{ fontFamily: 'PokemonFont, Arial, sans-serif' }}>COMING SOON</h1>
         <div className="center-content">
-          <h1 className="coming-soon-text">COMING SOON</h1>
-          <img
-            className="character-bike"
-            src={bikingGirl}
-            alt="Character on bike"
-          />
-          <img
-            className="character-pikachu"
-            src={runPikachu}
-            alt="Pikachu"
-          />
+          
+          <div className="character-container">
+
+            <img
+              className="character-pikachu"
+              src={runPikachu}
+              alt="Pikachu"
+            />
+            <img
+              className="character-bike"
+              src={bikingGirl}
+              alt="Character on bike"
+            />
+          </div>
         </div>
+
       </div>
 
 
